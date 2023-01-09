100307c25253f0aaf8396e37c6938cddfcc54b8d
Nilton Junior <ngm.junior@outlook.com>
2023-01-09 20:39:09 -0300

    hello world
    Signed-off-by: Nilton Junior <ngm.junior@outlook.com>

---
+++ hello/target/debug/deps/hello-18bf37c42f8fa4a6.d
@@ -0,0 +1,5 @@
+/home/nilton/projects/udemy-complete-rust/hello/target/debug/deps/hello-18bf37c42f8fa4a6: src/main.rs
+
+/home/nilton/projects/udemy-complete-rust/hello/target/debug/deps/hello-18bf37c42f8fa4a6.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/nilton/projects/udemy-complete-rust/hello/target/debug/hello: /home/nilton/projects/udemy-complete-rust/hello/src/main.rs
